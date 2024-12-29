5b0d492b511de68f42492dd7159cdcba7075930c
bali-1 <134980136+bali-1@users.noreply.github.com>
2024-12-29 11:47:40 -0600

    fixed profile pic pathing

--- src/components/about/ProfileImage.tsx
+++ src/components/about/ProfileImage.tsx
@@ -7,7 +7,7 @@ export const ProfileImage = () => {
     return (
         <div className="relative w-48 h-48 transition-transform duration-300 hover:scale-105">
             <Image
-                src={getAssetPath('/profilepicture.jpg')}
+                src={getAssetPath('./profilepicture.jpg')}
                 alt="Profile Image"
                 width={192}
                 height={192}
